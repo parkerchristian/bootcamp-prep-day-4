a3890001398f12896dd000c8cfe8b1fba2896b03
Parker Helland <hellandp@gmail.com>
2019-01-17 19:45:51 -0800

    making greeting!

--- main.js
+++ main.js
@@ -2,9 +2,19 @@
 function greet() {
     // get the inputs by id
 
+    var firstNameInput = document.getElementById('first-name');
+    var lastNameInput = document.getElementById('last-name');
+    
     // get values out of inputs
-
+    var firstName = firstNameInput.value;
+    var lastName = lastNameInput.value;
+    
+    
     // make greeting
+    
+    var greeting = 'Hello ' + firstName + ' ' + lastName + '!';
+    console.log(greeting);
+
 
     // display greeting
 
